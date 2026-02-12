879e9b18127ec6705ec5692a8d2f94d186d3ce94
rellimylime <ermiller95616@gmail.com>
2026-02-12 20:49:18 +0000

    Refactor get_stac_catalog_path function to use system.file for improved path retrieval and update documentation

--- R/stac_config.R
+++ R/stac_config.R
@@ -1,7 +1,29 @@
-#' Get the local STAC catalog path
+# Reference: R Packages (2e), Chapter 7.4: Internal data
+#   https://r-pkgs.org/data.html#sec-data-extdata
+
+#' Get the STAC catalog path
 #'
-#' @return Character path to the STAC catalog
+#' Returns the path to the STAC catalog shipped with the fedex package.
+#' The STAC catalog contains metadata about all available WRI layers.
+#'
+#' @return Character. Full path to the STAC items directory
+#'
+#' @details
+#' The STAC catalog is pre-generated and ships with the package in
+#' inst/extdata/stac/. This function uses system.file() to find the
+#' installed location.
+#'
+#' @examples
+#' # Get the path to the STAC catalog
+#' stac_path <- get_stac_catalog_path()
+#'
+#' # List STAC item files
+#' list.files(stac_path, pattern = "\\.json$")
+#'
 #' @export
 get_stac_catalog_path <- function() {
-  file.path("stac", "collections", "wri_ignitR", "items")
+  # Use system.file() to find installed extdata location
+  # This works whether package is installed or loaded with devtools::load_all()
+  system.file("extdata", "stac", "collections", "wri_ignitR", "items",
+              package = "fedex")
 }
